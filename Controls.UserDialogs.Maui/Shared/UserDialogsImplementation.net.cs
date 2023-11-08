bc8fd0826778e137fa60edeb03cb8ab7a0747859
ewerspej <ewerspej@tcd.ie>
2023-11-08 11:36:14 +0100

    added support for plain net6.0 and net7.0 platform targets

---
+++ Controls.UserDialogs.Maui/Shared/UserDialogsImplementation.net.cs
@@ -0,0 +1,39 @@
+﻿namespace Controls.UserDialogs.Maui;
+
+public partial class UserDialogsImplementation : IDisposable
+{
+    public virtual partial IDisposable Alert(AlertConfig config)
+    {
+        return this;
+    }
+
+    public virtual partial IDisposable Confirm(ConfirmConfig config)
+    {
+        return this;
+    }
+
+    public virtual partial IDisposable ActionSheet(ActionSheetConfig config)
+    {
+        return this;
+    }
+
+    public virtual partial IDisposable ShowToast(ToastConfig config)
+    {
+        return this;
+    }
+
+    public virtual partial IDisposable ShowSnackbar(SnackbarConfig config)
+    {
+        return this;
+    }
+
+    protected virtual partial IHudDialog CreateHudInstance(HudDialogConfig config)
+    {
+        return CurrentHudDialog;
+    }
+
+    public void Dispose()
+    {
+        CurrentHudDialog?.Dispose();
+    }
+}
